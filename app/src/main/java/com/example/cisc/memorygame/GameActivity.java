71b448b9a5303ea72b54423fe195c0e584f88050
James <jgappy@sdccd.edu>
2016-09-29 14:03:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cisc/memorygame/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.cisc.memorygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
