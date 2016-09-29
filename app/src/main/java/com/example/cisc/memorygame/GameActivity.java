5ff710117441631e9faa6d064f6c47e9bccd9f9e
James <jgappy@sdccd.edu>
2016-09-29 14:22:16 -0700

    removed title bar from main activity only

--- app/src/main/java/com/example/cisc/memorygame/GameActivity.java
+++ app/src/main/java/com/example/cisc/memorygame/GameActivity.java
@@ -2,8 +2,10 @@ package com.example.cisc.memorygame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class GameActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
